2eb6edddf1ad8fd40b76865bb48f660494412fd7
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-08-31 23:17:23 -0700

    Update b_io.h

--- b_io.h
+++ b_io.h
@@ -22,3 +22,11 @@ void b_init();
 
 b_io_fd b_open (char * filename, int flags);
 b_io_fd b_getFCB();
+
+int b_read (b_io_fd fd, char * buffer, int count);
+int b_write (b_io_fd fd, char * buffer, int count);
+int b_seek (b_io_fd fd, off_t offset, int whence);
+int b_close (b_io_fd fd);
+
+#endif
+
