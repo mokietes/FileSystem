8b052801ec2dca667dd4e4b02dcc9df94c1f3134
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-08-31 23:17:05 -0700

    Create b_io.h

---
+++ b_io.h
@@ -0,0 +1,24 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: b_io.h
+*
+* Description:: Interface of basic I/O Operations
+*
+**************************************************************/
+
+#ifndef _B_IO_H
+#define _B_IO_H
+#include <fcntl.h>
+
+typedef int b_io_fd;
+void b_init();
+
+
+b_io_fd b_open (char * filename, int flags);
+b_io_fd b_getFCB();
