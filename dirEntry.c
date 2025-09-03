9e1789e1de1450fdd0610ced217fd953f9e0ec15
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-02 20:19:34 -0700

    Create dirEntry.c

---
+++ dirEntry.c
@@ -0,0 +1,18 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: dirEntry.c
+*
+* Description:: Implements functionality for creating, 
+* initializing, loading, and saving directories in the custom 
+* file system.Creating new directories with createDir(), 
+* including setting . and .. entries. Initializing the root 
+* directory with initRootDir() and updating the global VCB.
+* Loading and saving the root directory from/to disk using 
+* loadRootDir() and saveRootDir().
+**************************************************************/
