7d753ae5467c905fd92e4a15107cbc8c4086990c
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-02 20:10:09 -0700

    Create bitmap.h

---
+++ bitmap.h
@@ -0,0 +1,14 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: bitmap.h
+*
+* Description:: This header file contains the function
+* prototypes for modifying a bit in the free space bitmap.
+*
+**************************************************************/
