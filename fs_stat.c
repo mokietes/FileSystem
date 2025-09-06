87030a53c14280bdde56ed672d49492ed7049706
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-05 22:22:17 -0700

    Create fs_stat.c

---
+++ fs_stat.c
@@ -0,0 +1,22 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: fs_stat.c
+*
+* Description:: This file implements the function fs_stat.
+*
+*
+**************************************************************/
+
+#include <string.h>
+
+
+#include "mfs.h"
+#include "parsePath.h"
+#include "vcb.h"
+
