2c5900d61735866fae4f457187276b1cdb1861cb
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-05 22:21:51 -0700

    Create dirOps.c

---
+++ dirOps.c
@@ -0,0 +1,18 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: dirOps.c
+*
+* Description:: Implements directory operations including iteration,
+* path utilities, and directory management functions for the custom
+* file system. This includes fs_opendir, fs_readdir, fs_closedir,
+* fs_getcwd, fs_setcwd, fs_isFile, fs_isDir, fs_mkdir, fs_rmdir,
+* and fs_delete functions.
+*
+**************************************************************/
+
