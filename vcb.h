6f0b9e8532de221c068f633d1c03090bb25abdd0
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-07 00:27:52 -0700

    Create vcb.h

---
+++ vcb.h
@@ -0,0 +1,18 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien
+* Student IDs:: 923752034 , 923676946
+* GitHub-Name:: thats-not-my-name , mokietes
+* Group-Name:: Epsilon
+* Project:: Epsilon File System
+*
+* File:: vcb.h
+*
+* Description:: This header file contains the VCB struct 
+* definition and VCB function prototypes.
+*
+**************************************************************/
+
+#ifndef VCB_H
+#define VCB_H
+
