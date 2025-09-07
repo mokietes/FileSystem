d918112f5552b1aee06281ad1d87d1d154d2b5d6
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-07 00:27:32 -0700

    Create parsePath.h

---
+++ parsePath.h
@@ -0,0 +1,21 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: parsePath.h
+*
+* Description:: This header file contains the struct definition
+* for the parse path information and the function prototypes
+* for the functions needed to parse a pathname.
+*
+**************************************************************/
+
+#ifndef PARSE_PATH_H
+#define PARSE_PATH_H
+
+#include "dirEntry.h"
+
