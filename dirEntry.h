8a567cd4826c4a058d9e7171d6317b705e2abc8c
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-08 20:38:10 -0700

    Update dirEntry.h

--- dirEntry.h
+++ dirEntry.h
@@ -21,3 +21,14 @@
 #define MAX_NAME 15
 #define DEFAULT_ENTRIES 50
 #define ROOT_ENTRIES_MULT 4
+
+typedef struct dirEntry {
+    char name[MAX_NAME + 1]; // filename or dir name
+    time_t createTime;
+    time_t modifyTime;
+    time_t accessTime;
+    int size;                // file size in bytes (0 for directories)
+    int blockLoc;            // starting block
+    char isDir;              // 1 = dir, 0 = file
+} dirEntry;
+
