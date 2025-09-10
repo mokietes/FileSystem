46f844c775c74a9861d54e4830c54841da99b410
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-09 23:05:06 -0700

    Create dirFunc.c

---
+++ dirFunc.c
@@ -0,0 +1,16 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: dirFunc.c
+*
+* Description:: This file contains implementations for the
+* fs_mkdir and fs_rmdir functions as well as related helper
+* functions.
+*
+**************************************************************/
+
