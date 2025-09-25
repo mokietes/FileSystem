69eba4d0e915c690ff465050b9f1064f6442dc7d
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-25 01:02:12 -0700

    Create fsInit.c

---
+++ fsInit.c
@@ -0,0 +1,16 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: fsInit.c
+*
+* Description:: Main driver for file system assignment.
+*
+* This file is where you will start and initialize your system
+*
+**************************************************************/
+
