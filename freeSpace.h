73cc4018e98af7de71f5e7bb4814359300c1c3e3
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-09-30 00:14:08 -0700

    Create freeSpace.h

---
+++ freeSpace.h
@@ -0,0 +1,15 @@
+/**************************************************************
+* Class::  CSC-415-01 Summer 2025
+* Name:: Derek Yang, Mekonnen Tesfazien, Andrew Brockenborough
+* Student IDs:: 923752034 , 923676946 , 921749371
+* GitHub-Name:: thats-not-my-name , mokietes, andrewb-03
+* Group-Name:: Team Epsilon
+* Project:: Epsilon File System
+*
+* File:: freeSpace.h
+*
+* Description:: This header file contains the constants and
+* function prototypes for free space management.
+*
+**************************************************************/
+
