00d331588a96b44796ad1466699c2aeaaa0bbdc7
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-10-18 22:54:11 -0700

    Update fsshell.c

--- fsshell.c
+++ fsshell.c
@@ -15,3 +15,22 @@
 *
 **************************************************************/
 
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <getopt.h>
+#include <string.h>
+
+#include "fsLow.h"
+#include "mfs.h"
+
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
+
+#define SINGLE_QUOTE	0x27
+#define DOUBLE_QUOTE	0x22
+#define BUFFERLEN		200
+#define DIRMAX_LEN		4096
