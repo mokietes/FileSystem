c00f015c97047fbb2b7f4004e0a752bc95292b15
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-10-20 22:41:18 -0700

    Create mfs.h

---
+++ mfs.h
@@ -0,0 +1,17 @@
+/**************************************************************
+* Class::  CSC-415-0# Spring 2024
+* Name::
+* Student IDs::
+* GitHub-Name::
+* Group-Name::
+* Project:: Basic File System
+*
+* File:: mfs.h
+*
+* Description:: 
+*	This is the file system interface.
+*	This is the interface needed by the driver to interact with
+*	your filesystem.
+*
+**************************************************************/
+
