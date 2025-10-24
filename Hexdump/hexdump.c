e23d9e8cc40963e7c31c3afde8448ea929657dea
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-10-23 23:46:47 -0700

    Create hexdump.c

---
+++ Hexdump/hexdump.c
@@ -0,0 +1,17 @@
+/****************************************************************************
+ *	hexdump - a program to display a file in hexadecimal and ascii
+ *	Such as:
+ *		000020: 19 00 00 00 48 00 00 00  5F 5F 50 41 47 45 5A 45 | ....H...__PAGEZE
+ *
+ *  Author:		Robert Bierman
+ *	Date:		March 28, 2020
+ *	Source:		hexdump.c
+ *
+ *  Primary Purpose:
+ *	Written as a utility for CSC-415 Operating Systems, File System project
+ *  to allow the dumping of the "drive" file for verification of proper 
+ *  structure and content.
+ *
+ *  copyright 2020 Robert Bierman
+ ****************************************************************************/
+
