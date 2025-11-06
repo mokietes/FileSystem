73f02faf7eb30eab034c932836a30cf29b6957bf
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-11-05 23:28:34 -0800

    Update mfs.h

--- mfs.h
+++ mfs.h
@@ -40,3 +40,20 @@ typedef u_int64_t uint64_t;
 typedef u_int32_t uint32_t;
 #endif
 
+
+
+
+// This structure is returned by fs_readdir to provide the caller with information
+// about each file as it iterates through a directory
+struct fs_diriteminfo
+	{
+    unsigned short d_reclen;    /* length of this record */
+    unsigned char fileType;    
+    char d_name[256]; 			/* filename max filename is 255 characters */
+	};
+
+// This is a private structure used only by fs_opendir, fs_readdir, and fs_closedir
+// Think of this like a file descriptor but for a directory - one can only read
+// from a directory.  This structure helps you (the file system) keep track of
+// which directory entry you are currently processing so that everytime the caller
+// calls the function readdir, you give the next entry in the directory
