5db67d63e22e10f4f2e4fc7f3b6099f3f57d532d
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2025-11-05 23:28:41 -0800

    Update mfs.h

--- mfs.h
+++ mfs.h
@@ -43,6 +43,7 @@ typedef u_int32_t uint32_t;
 
 
 
+
 // This structure is returned by fs_readdir to provide the caller with information
 // about each file as it iterates through a directory
 struct fs_diriteminfo
@@ -57,3 +58,12 @@ struct fs_diriteminfo
 // from a directory.  This structure helps you (the file system) keep track of
 // which directory entry you are currently processing so that everytime the caller
 // calls the function readdir, you give the next entry in the directory
+typedef struct fdDir
+	{
+	/*****TO DO:  Fill in this structure with what your open/read directory needs  *****/
+	unsigned short  d_reclen;		/* length of this record */
+	unsigned short	dirEntryPosition;	/* which directory entry position, like file pos */
+	dirEntry *	directory;			/* Pointer to the loaded directory you want to iterate */
+	struct fs_diriteminfo * di;		/* Pointer to the structure you return from read */
+	} fdDir;
+
